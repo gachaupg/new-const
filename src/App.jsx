9cda8849baa7558936c4ca4e96f6de81d972e8df
Ebenezer Okello <okelloebenezer403@gmail.com>
2023-07-21 20:38:15 +0300

    ddd

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Link,BrowserRouter,Routes,Route } from 'react-router-dom'
 import Services from './pages/Services';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       hello
       <BrowserRouter>
+      <Navbar/>
       <Routes>
 <Route path='/services' element={<Services/>}/>
       </Routes>
